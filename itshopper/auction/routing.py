82d0796ad4ee07121d28cc3632c5fbb5df88731f
federicosia <federicosiano1@gmail.com>
2024-07-07 22:26:41 +0200

    - reorder project structure - rewrite requirements.txt with pipreqs - add Dockerfile - update compose.yml

--- itshopper/auction/routing.py
+++
@@ -1,8 +0,0 @@
-# auction/routing.py
-from django.urls import re_path
-
-from auction.consumers import AuctionConsumer
-
-websocket_urlpatterns = [
-    re_path(r"auction", AuctionConsumer.as_asgi()),
-]
--- itshopper/order/repositories/orders.py
+++
@@ -1,6 +0,0 @@
-from .base import BaseRepository
-from order.models import Order
-
-
-class OrderRepository(BaseRepository):
-    model = Order
